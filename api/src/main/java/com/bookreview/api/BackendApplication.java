bd935f02a5e959f5644a79bcbd9eeb92ba5c6144
DJ Hogarth <dariyhogarth@hotmail.com>
2023-02-06 13:43:36 -0500

    cleaned up BackendApplication annotations

--- api/src/main/java/com/bookreview/api/BackendApplication.java
+++ api/src/main/java/com/bookreview/api/BackendApplication.java
@@ -2,9 +2,13 @@ package com.bookreview.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+// import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication()
+//@ComponentScan({"com.bookreview.api.user.UserRepository"})
+//@EnableJpaRepositories("com.bookreview.api.user.UserRepository")
 public class BackendApplication {
 
 	public static void main(String[] args) {
