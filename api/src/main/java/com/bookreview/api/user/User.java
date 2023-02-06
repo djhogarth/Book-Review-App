c17ce91d599c3b2a2f66a93d5c1f3d88ba43a822
DJ Hogarth <dariyhogarth@hotmail.com>
2023-02-06 13:46:40 -0500

    cleaned up User class

--- api/src/main/java/com/bookreview/api/user/User.java
+++ api/src/main/java/com/bookreview/api/user/User.java
@@ -1,14 +1,13 @@
 package com.bookreview.api.user;
 
+import java.util.UUID;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+// import org.hibernate.validator.constraints.UUID;
 
-import org.hibernate.annotations.UuidGenerator;
-import org.hibernate.validator.constraints.UUID;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
@@ -16,7 +15,6 @@ import lombok.Data;
 public class User {
 	@Id
 	@GeneratedValue
-	@UuidGenerator
 	private UUID Id;
 	private String username;
 	private String displayName;
@@ -24,6 +22,5 @@ public class User {
 	private String bio;
 	private String country;
 	private String city;
-	
 
 }
