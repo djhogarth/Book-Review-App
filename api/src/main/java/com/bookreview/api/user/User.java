6abef83aadf56bba9493ea094e25088e4844c3f7
DJ Hogarth <dariyhogarth@hotmail.com>
2023-02-05 23:18:38 -0500

    add a primary key for the user class and we give it the Entity Annotation to tell spring that this a database Entity

--- api/src/main/java/com/bookreview/api/user/User.java
+++ api/src/main/java/com/bookreview/api/user/User.java
@@ -1,9 +1,23 @@
 package com.bookreview.api.user;
 
+
+
+
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.validator.constraints.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class User {
+	@Id
+	@GeneratedValue
+	@UuidGenerator
+	private UUID Id;
 	private String username;
 	private String displayName;
 	private String password;
