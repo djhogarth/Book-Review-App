b861a939ff49b08822a6903c2919384be52e5139
DJ Hogarth <dariyhogarth@hotmail.com>
2023-02-06 18:03:54 -0500

    Solved the issue of the users table not being created

--- api/src/main/java/com/bookreview/api/user/User.java
+++ api/src/main/java/com/bookreview/api/user/User.java
@@ -5,17 +5,20 @@ import java.util.UUID;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 // import org.hibernate.validator.constraints.UUID;
 
 import lombok.Data;
 
-@Data
 @Entity
+@Table(name="Users")
+@Data
 public class User {
 	@Id
 	@GeneratedValue
 	private UUID Id;
+	
 	private String username;
 	private String displayName;
 	private String password;
