9bbad613421a48ff29e571e35e27f52472a7f31f
DJ Hogarth <dariyhogarth@hotmail.com>
2023-02-05 23:20:44 -0500

    created a test that confirms if a user has been added to the database. Also refactored code that creates an valid user object and extracted out the http user path to a constant to avoid repeating code.

--- api/src/test/java/com/bookreview/api/UserControllerTest.java
+++ api/src/test/java/com/bookreview/api/UserControllerTest.java
@@ -18,24 +18,37 @@ import com.bookreview.api.user.User;
 
 public class UserControllerTest {
 	
+private static final String API_USERS_PATH = "/api/1.0/users";
 @Autowired
 	TestRestTemplate testRestTemplate;
 	
 @Test
 	public void postUser_whenUserIsValid_receiveOk( ) {
-		User appUser = new User();
-//		test if the user name and display name are at least 4 characters long
-		appUser.setUsername("testUser");
-		appUser.setDisplayName("testdisplay");
-//		test if password has at least one uppercase, one lowercase and one numeric
-		appUser.setPassword("P4ssword");
-//		test if the rest of fields have at least 4 characters
-		appUser.setBio("loremIpsum12342");
-		appUser.setCountry("Canada");
-		appUser.setCity("Mississauga");
+		User appUser = createValidUser();
 		
-		ResponseEntity<Object> response = testRestTemplate.postForEntity("/api/1.0/users", appUser, Object.class);
+		ResponseEntity<Object> response = testRestTemplate.postForEntity(API_USERS_PATH, appUser, Object.class);
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
+
+private User createValidUser() {
+	User appUser = new User();
+//		test if the user name and display name are at least 4 characters long
+	appUser.setUsername("testUser");
+	appUser.setDisplayName("testdisplay");
+//		test if password has at least one uppercase, one lowercase and one numeric
+	appUser.setPassword("P4ssword");
+//		test if the rest of fields have at least 4 characters
+	appUser.setBio("loremIpsum12342");
+	appUser.setCountry("Canada");
+	appUser.setCity("Mississauga");
+	return appUser;
+}
+
+@Test
+	public void postUser_whenUserIsValid_SavedUserToDatabase() {
+	User appUser = createValidUser();
+	testRestTemplate.postForEntity(API_USERS_PATH, appUser, Object.class);
+	assertThat(userRepository.count()).isEqualTo(1);
+}	
 
 }
