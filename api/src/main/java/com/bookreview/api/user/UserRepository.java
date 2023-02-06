c9c2e59e6335539629ae633f4d2430df8d04ddd9
DJ Hogarth <dariyhogarth@hotmail.com>
2023-02-06 13:47:56 -0500

    changed UUID import

--- api/src/main/java/com/bookreview/api/user/UserRepository.java
+++ api/src/main/java/com/bookreview/api/user/UserRepository.java
@@ -1,8 +1,11 @@
 package com.bookreview.api.user;
 
 
-import org.hibernate.validator.constraints.UUID;
+import java.util.UUID;
+
+// import org.hibernate.validator.constraints.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
